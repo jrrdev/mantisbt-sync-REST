4b8aac7919f60a307f94a5a0b7b30683c33826a3
jrrdev <devarulg@hotmail.fr>
2017-01-19 12:41:09 +0100

    Creates separated test case to test projections

--- src/test/java/com/github/jrrdev/mantisbtsync/rest/domain/projections/BugSummaryTest.java
+++ src/test/java/com/github/jrrdev/mantisbtsync/rest/domain/projections/BugSummaryTest.java
@@ -1,7 +1,7 @@
 /**
  *
  */
-package com.github.jrrdev.mantisbtsync.rest;
+package com.github.jrrdev.mantisbtsync.rest.domain.projections;
 
 import static com.ninja_squad.dbsetup.Operations.insertInto;
 import static com.ninja_squad.dbsetup.Operations.sequenceOf;
@@ -28,7 +28,7 @@ import com.ninja_squad.dbsetup.operation.Operation;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class BugRestTest extends AbstractRepositoryTest {
+public class BugSummaryTest extends AbstractRepositoryTest {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -67,47 +67,4 @@ public class BugRestTest extends AbstractRepositoryTest {
 				.andExpect(jsonPath("$.handlerName").value("target user"))
 				.andExpect(jsonPath("$.statusName").value("target status"));
 	}
-
-	@Test
-	public void testFindByProjectIdAndStatusIdNotIn() throws Exception {
-		final Operation op = sequenceOf(
-				insertInto("mantis_enum_status")
-				.columns("id", "name")
-				.values(1, "target status")
-				.values(2, "other status")
-				.build(),
-
-				insertInto("mantis_project_table")
-				.columns("id", "name")
-				.values(1, "target project")
-				.values(2, "other project")
-				.build(),
-
-				insertInto("mantis_user_table")
-				.columns("id", "name")
-				.values(1, "target user")
-				.values(2, "other user")
-				.build(),
-
-				insertInto("mantis_bug_table")
-				.columns("id", "project_id", "status_id", "handler_id", "summary", "description", "last_sync")
-				.values(1, 1, 1, 1, "should be in result", "description 1", ValueGenerators.dateSequence().nextValue())
-				.values(2, 2, 1, 1, "wrong project", "description 2", ValueGenerators.dateSequence().nextValue())
-				.values(3, 1, 1, 2, "should be in result 2", "description 3", ValueGenerators.dateSequence().nextValue())
-				.values(4, 1, 2, 1, "wrong status", "description 4", ValueGenerators.dateSequence().nextValue())
-				.build());
-
-		lauchOperation(op);
-
-		mockMvc.perform(
-				get("/bugs/search/findByProjectIdAndStatusIdNotIn"
-						+ "?project={id}&status={status}&sort=id,asc",
-						1L, 2L))
-						.andExpect(status().isOk())
-						.andExpect(jsonPath("$._embedded.bugs[0].summary").value("should be in result"))
-						.andExpect(jsonPath("$._embedded.bugs[0].description").value("description 1"))
-						.andExpect(jsonPath("$._embedded.bugs[1].summary").value("should be in result 2"))
-						.andExpect(jsonPath("$._embedded.bugs[1].description").value("description 3"));
-	}
-
 }
